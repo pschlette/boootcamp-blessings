3dd86dc81e857bb166d90c2d2c4b1673f214371c
Pete Schlette <pete.schlette@gmail.com>
2019-07-10 23:46:45 -0500

    More build config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-export const helloWorld = (req: Request, res: Response) => {
+exports.helloWorld = (req: Request, res: Response) => {
   let message = req.query.message || req.body.message || "Hello World!";
   res.status(200).send(message);
 };
