8aac42a5b9cacb6c1b9811b12e22e84934cf9fe9
Pete Schlette <pete.schlette@gmail.com>
2019-07-14 15:33:13 -0500

    Reset test database before each end-to-end test

--- src/e2e-tests/smoke.test.ts
+++ src/e2e-tests/smoke.test.ts
@@ -1,15 +1,20 @@
 import axios from "axios";
+import { resetTestDb } from "../test-utils";
 
 describe("End-to-end smoke test", () => {
   const TEST_SERVER_URL = "http://localhost:8000";
   const testAxios = axios.create({ baseURL: TEST_SERVER_URL });
 
+  beforeEach(async () => {
+    await resetTestDb();
+  });
+
   it("can request data from test server with test db", async () => {
     expect.assertions(2);
-    const result = await testAxios.get("http://localhost:8000/?test=yes");
+    const result = await testAxios.get("/?test=yes");
     expect(result.data).toBe("The bless count is 0");
 
-    const result2 = await testAxios.get("http://localhost:8000/?test=yes");
+    const result2 = await testAxios.get("/?test=yes");
     expect(result2.data).toBe("The bless count is 1");
   });
 });
