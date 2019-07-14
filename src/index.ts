94848f56a4943d8a351700b8da6eef830290f75b
Pete Schlette <pete.schlette@gmail.com>
2019-07-14 15:32:56 -0500

    Improve detection of requests from functions framework startup in server, clean up throwaway code a little

--- src/index.ts
+++ src/index.ts
@@ -23,19 +23,22 @@ if (envFilename) {
 exports.blessings = async (req: Request, res: Response) => {
   console.log(`Environment is ${process.env.NODE_ENV}`);
 
-  console.log(JSON.stringify(req.query, null, 2));
-
-  if (!req.query.test) {
+  // For some reason the google cloud functions emulator sends some empty
+  // requests to the server when it starts up. Respond with a blank 200
+  if (!req.headers["accept"]) {
     res.status(200).send();
     return;
   }
 
+  // TODO Parse Slack's slash command request
+
+  // Test interacting with Datastore
   const dbClient = getDbClient();
 
-  const transaction = dbClient.transaction();
-  const userKey = dbClient.key(["User", "@capn_hoops"]);
-  console.log(`User key path: ${userKey.path}`);
+  const userKey = dbClient.key(["TestUser", "@capn_hoops"]);
 
+  // Read and update a test count within a transaction
+  const transaction = dbClient.transaction();
   await transaction.run();
   const [data] = (await transaction.get(userKey)) as Array<null | {
     count: number;
@@ -48,6 +51,4 @@ exports.blessings = async (req: Request, res: Response) => {
     }
   });
   await transaction.commit();
-
-  // let message = req.query.message || req.body.message || "Hello World!";
 };
